e1ce4c7cb9c3a8aafdc284c3e50cf2ce5379c18c
Bald Mike <baldmike312@gmail.com>
2017-07-17 21:47:02 -0500

    Ninja Got Gold

--- amadon/apps/amadon_site/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from views import index, checkout, reset
-
-urlpatterns = [
-    url(r'^$', index),
-    url(r'^checkout$', checkout),
-    url(r'^reset$', reset)
-]
